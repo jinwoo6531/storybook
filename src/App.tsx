f235288086899a0d5f00580d4e1814e3a76d72e6
jinwoo <jintakk6531@gmail.com>
2020-12-09 21:42:31 +0900

    12/9

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Box from './components/Box/Box';
 import Grid from './components/Grid/Grid';
+import GridRuler from './components/GridRuler/GridRuler';
 
 function App() {
   return (
-    <div className="App">
-      <Grid container cols={1}>
-        <Grid item>
-        <Box>Box 1</Box>
+    <div className="App" style={{ margin: '16px', position: 'relative' }}>
+      <GridRuler spacing="sm"></GridRuler>
+
+      <Grid container spacing="sm">
+        <Grid item cols={6}>
+          <Box>Box 1</Box>
         </Grid>
 
-        <Grid item cols={1}>
-        <Box>Box 2</Box>
+        <Grid item cols={6}>
+          <Box>Box 2</Box>
         </Grid>
 
-        <Grid item cols={1}>
-        <Box>Box 3</Box>
+        <Grid item cols={6}>
+          <Box>Box 3</Box>
+        </Grid>
+
+        <Grid item cols={6}>
+          <Box>Box 4</Box>
         </Grid>
       </Grid>
     </div>
--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,28 +1,25 @@
-import React,{FunctionComponent,ReactNode} from 'react'
+import React, { FunctionComponent, ReactNode } from 'react';
 import styles from './Grid.module.scss';
 import cn from 'classnames';
 
 type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type Spacing = 'sm' | 'md' | 'lg';
 
+const Grid: FunctionComponent<{
+  children: ReactNode;
+  container?: boolean;
+  item?: boolean;
+  cols?: Cols;
+  spacing?: Spacing;
+}> = ({ children, container, item, cols, spacing }) => {
+  const classNames = cn({
+    [styles.Grid_container]: container,
+    [styles.Grid_item]: item,
+    [styles[`Grid_cols_${cols}`]]: cols, //열 조절
+    [styles[`Grid_spacing_${spacing}`]]: spacing, //간격 조절
+  });
 
-const Grid:FunctionComponent<{
-    children:ReactNode;
-    container?: boolean;
-    item? : boolean;
-    cols? : Cols;
-}> = ({children,container,item,cols}) => {
+  return <div className={classNames}>{children}</div>;
+};
 
-    const classNames = cn({
-        [styles.Grid_container]: container,
-        [styles.Grid_item]: item,
-        [styles.Grid_cols]: cols,
-    })
-
-    return (
-        <div className={classNames}>
-         {children}   
-        </div>
-    )
-}
-
-export default Grid
+export default Grid;
